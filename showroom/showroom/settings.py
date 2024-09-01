7bf6e1872266997c15362cc591da9433603b3194
Fabian <fa.geiger@enbw.com>
2024-09-01 11:41:40 +0200

    Update

--- showroom/showroom/settings.py
+++ showroom/showroom/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'crispy-daisyui/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
