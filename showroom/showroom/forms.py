970e1cd63b023077c2559b49dc59b2bfd526370a
Fabian <geiger.fabian@outlook.com>
2025-12-07 11:42:52 +0100

    Add multi-select examples to showroom

--- showroom/showroom/forms.py
+++ showroom/showroom/forms.py
@@ -4,6 +4,18 @@ class Form1(forms.Form):
     name = forms.CharField(label='Name')
     email = forms.EmailField(label='Email')
     select = forms.ChoiceField(choices=[('1', 'One'), ('2', 'Two'), ('3', 'Three')])
+    multi_select = forms.MultipleChoiceField(
+        label='Multi-select',
+        choices=[('1', 'Option One'), ('2', 'Option Two'), ('3', 'Option Three')],
+        initial=['2'],
+        help_text='Hold Ctrl/Cmd to select multiple options'
+    )
+    multi_select_no_initial = forms.MultipleChoiceField(
+        label='Multi-select (no initial)',
+        choices=[('a', 'Alpha'), ('b', 'Beta'), ('c', 'Gamma')],
+        required=False,
+        help_text='Optional field, no pre-selected values'
+    )
     checkbox = forms.BooleanField(label='Checkbox')
     remember_me = forms.BooleanField(
         label='toggle field',
