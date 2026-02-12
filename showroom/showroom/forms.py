0b4f4d0cb8f228de22fb9b9d0446abd512fb71d2
Fabian <geiger.fabian@outlook.com>
2026-02-12 20:34:44 +0100

    Fix NullBooleanField rendering and add optgroup support
    Use optgroups filter instead of field.field.choices for select
    rendering. Fixes empty select for NullBooleanField and adds
    support for grouped choices (optgroup).
    
    Closes #15

--- showroom/showroom/forms.py
+++ showroom/showroom/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 
+
 class Form1(forms.Form):
     name = forms.CharField(label='Name')
     email = forms.EmailField(label='Email')
@@ -8,18 +9,18 @@ class Form1(forms.Form):
         label='Multi-select',
         choices=[('1', 'Option One'), ('2', 'Option Two'), ('3', 'Option Three')],
         initial=['2'],
-        help_text='Hold Ctrl/Cmd to select multiple options'
+        help_text='Hold Ctrl/Cmd to select multiple options',
     )
     multi_select_no_initial = forms.MultipleChoiceField(
         label='Multi-select (no initial)',
         choices=[('a', 'Alpha'), ('b', 'Beta'), ('c', 'Gamma')],
         required=False,
-        help_text='Optional field, no pre-selected values'
+        help_text='Optional field, no pre-selected values',
     )
     checkbox = forms.BooleanField(label='Checkbox')
     remember_me = forms.BooleanField(
         label='toggle field',
-        widget=forms.CheckboxInput(attrs={'class': 'toggle'})
+        widget=forms.CheckboxInput(attrs={'class': 'toggle'}),
     )
     radio = forms.ChoiceField(choices=[('1', 'One'), ('2', 'Two'), ('3', 'Three')], widget=forms.RadioSelect)
     textarea = forms.CharField(label='Textarea', widget=forms.Textarea)
@@ -28,4 +29,4 @@ class Form1(forms.Form):
     time = forms.TimeField(label='Time')
     file = forms.FileField(label='File')
     image = forms.ImageField(label='Image')
-
+    nullable_bool = forms.NullBooleanField(label='Nullable Bool')
